b570a5a613c62136b1fcde09ea5d71d7062a8b9a
mayingcong <mayingcong@xingxy.cn>
2016-06-27 22:41:47 +0800

    1. add comments 2. delete "version" property 3. remove authServiceProvider configuration 4. add real $http.post() method to validate user

--- app/auth/authServiceProvider.provider.js
+++ app/auth/authServiceProvider.provider.js
@@ -1,22 +1,30 @@
 'use strict';
 
+// 定义authServiceProvider 以及 authService
 angular.module('ng-normal-auth').provider('authService', function() {
     var defaults = {
-        version: '',
         submitLoginUrl: 'dataBase/user.json',
         loginFormUrl: 'auth/login/loginForm.html',
     };
 
     this.configure = function(options) {
+        // 扩展defaults
         angular.extend(defaults, options);
     };
 
+    // 实现Provider的$get方法
     this.$get = ['$http', 'AUTH_EVENTS', '$rootScope', 'Session', '$window', function($http, AUTH_EVENTS, $rootScope, Session, $window) {
         return {
             configs: defaults,
-            nextStateName: '',
+            nextStateName: '', // 记录认证之前的state状态，方便认证成功后跳转
+
+            // 登录认证方法
             login: function(params, url) {
                 url = url || this.configs.submitLoginUrl;
+
+                // 这里用$http.get()方法获取data.json里面的所有用户数据
+                // 是一种便于没有数据库时候的调试，实际项目中会将数据post
+                // 到server端，然后在server端进行校验 
                 return $http.get(url).then(function(res) {
                     var i = 0,
                         flag = false,
@@ -38,7 +46,21 @@ angular.module('ng-normal-auth').provider('authService', function() {
                         $rootScope.$broadcast(AUTH_EVENTS.loginFailed, res);
                     }
                     return flag && value;
+                }, function(err) {
+                    console.log(err);
+                    $rootScope.$broadcast(AUTH_EVENTS.loginFailed, err);
                 });
+
+                // 一下代码更像实际中使用的代码
+                // $http.post(url, params).then(function(res) {
+                //     Session.create(res.data.id, res.data.userRole);
+                //     $window.sessionStorage.setItem('userId', Session.userId);
+                //     $window.sessionStorage.setItem('userRole', Session.userRole);
+                //     $rootScope.$broadcast(AUTH_EVENTS.loginSuccess, res);
+                // }, function(err) {
+                //     console.log(err);
+                //     $rootScope.$broadcast(AUTH_EVENTS.loginFailed, err);
+                // })
             },
             isAuthenticated: function() {
                 return !!Session.userId;
@@ -51,12 +73,4 @@ angular.module('ng-normal-auth').provider('authService', function() {
             }
         }
     }];
-}).config(['authServiceProvider', function(authServiceProvider) {
-    authServiceProvider.configure({
-        version: '0.0.1',
-        submitLoginUrl: 'dataBase/user.json'
-
-    });
-}]).controller('authTestController', function(authService, $scope) {
-    $scope.version = authService.configs.version;
 });

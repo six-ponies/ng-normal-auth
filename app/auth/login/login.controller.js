a82a9bd168167b9a71f77df7ab1e2cdb485e90a9
mayingcong <mayingcong@xingxy.cn>
2016-06-27 22:58:31 +0800

    add comments

--- app/auth/login/login.controller.js
+++ app/auth/login/login.controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// 登录页面控制器
 angular.module('ng-normal-auth').controller('loginController', function($scope, $rootScope, authService, $state, AUTH_EVENTS) {
     $scope.credentials = {
         username: '',
@@ -9,6 +10,7 @@ angular.module('ng-normal-auth').controller('loginController', function($scope, 
         authService.login(credentials).then(function(user) {
             if (user) {
                 $scope.setCurrentUser(user);
+                // 返回到认证之前的用户想要去的页面
                 $state.go(authService.nextStateName);
             }
         });

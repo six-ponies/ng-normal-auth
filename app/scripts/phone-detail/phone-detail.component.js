be85b1053a6ab529d60a17f9885cf56192681312
mayingcong <mayingcong@xingxy.cn>
2016-06-27 21:47:24 +0800

    add phoneDetail module and its belongings. This is also an example from AngularJs website

---
+++ app/scripts/phone-detail/phone-detail.component.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.
+module('phoneDetail').
+component('phoneDetail', {
+    template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
+    controller: ['$routeParams',
+        function PhoneDetailController($routeParams) {
+            this.phoneId = $routeParams.phoneId;
+        }
+    ]
+});
